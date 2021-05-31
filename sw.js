f7e75a3f75bd68c06b9b75d1ed1e05e37d041092
khmilevoi <Khmilevoi@gmail.com>
2021-05-31 06:45:24 +0000

    Deploying to gh-pages from  @ eb6522a88a4ee85ec65298cc695c586df1aef54c 🚀

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
 /* eslint-disable no-restricted-globals */
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-        navigator.serviceWorker.register("sw.js").then((registration) => {
-            console.log("Service Worker registration successful: ", registration)
-        }, (err) => {
-            console.log("Registration failed", err)
-        })
-    })
+        navigator.serviceWorker.register("sw.js").then(
+            (registration) => {
+                console.log(
+                    "Service Worker registration successful: ",
+                    registration
+                );
+            },
+            (err) => {
+                console.log("Registration failed", err);
+            }
+        );
+    });
 }
 
 const createPath = (path) => {
@@ -16,43 +22,40 @@ const createPath = (path) => {
 
     const [name] = location.pathname.slice(1).split("/");
 
-    return `/${name}${path}`
-}
+    return `/${name}${path}`;
+};
 
-const cacheName = "neural-food"
+const cacheName = "cache";
 
-let paths = [
-    createPath("/index.html"),
-    createPath("/"),
-    createPath("/sw.js"),
-    createPath("/manifest.json"),
-]
+let paths = [createPath("/"), createPath("/sw.js")];
 
 self.addEventListener("install", (e) => {
-    e.waitUntil(caches.open(cacheName).then((cache) => {
-        return cache.addAll(paths)
-    }))
-})
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(paths);
+        })
+    );
+});
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith((async () => {
-        const cache = await caches.open(cacheName);
-        const cachedFiles = await cache.match(event.request);
+    event.respondWith(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            const cachedFiles = await cache.match(event.request);
 
-        try {
-            const response = await fetch(event.request);
+            try {
+                const response = await fetch(event.request);
 
-            await cache.put(event.request, response.clone());
+                await cache.put(event.request, response.clone());
 
-            return response
-        } catch (error) {
-            if (cachedFiles) {
-                return cachedFiles
-            } else {
-                throw error
+                return response;
+            } catch (error) {
+                if (cachedFiles) {
+                    return cachedFiles;
+                } else {
+                    throw error;
+                }
             }
-        }
-
-    })())
-
-})
+        })()
+    );
+});
